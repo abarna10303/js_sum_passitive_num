2aa361ac1913762ca4a99d7a90982a709dacd24b
abarna10303 <abarnas361@gmail.com>
2022-12-14 12:52:12 +0530

    modify

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
-var num=parseInt(prompt("how many times loop"));
+var num=parseInt(prompt("How many times"));
+var sum=0;
 for(i=0;i<num;i++)
 {
-    var num1=parseInt(prompt("Enter the Number1"));
-    var num2=parseInt(prompt("Enter the Number2"));
-    var num3=parseInt(prompt("Enter the Number3"));
-    var sum=0;
-   if(num1>0 && num2>0 && num3>0)
+    var num1=parseInt(prompt("Enter the Number"));
+   
+   if(num1>0)
    {
-        sum=num1+num2+num3;
-        document.write("The sum of possitive Number is : "+sum);
-        document.write("<br>");
-   }
-   else
-   {
-        document.write("This is a Nagative Numbers");
+        sum=sum+num1;
    }
 }
+document.write("The sum of possitive Number is : "+sum);
+
